4cc90e938c7a06e768168e2b4600fe07757d41de
Larik7 <Larik777taganka@gmail.com>
2023-03-16 02:18:31 +0200

    third commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,62 @@
 import React, { Component } from "react";
-import { Formik } from 'formik';
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
+import { Layout } from "./Layout";
+
+
 
 
 export class App extends Component {
   state = {
-  contacts: [],
-  filter: ''
+    contacts: [],
+    filter: ''
+  }
+
+  addContact = newContact => {
+    const findName = newContact.name.toLowerCase();
+    if (this.state.contacts.find(({ name }) => name.toLowerCase() === findName)) {
+      alert(`${newContact.name} is already in contacts`);
+      return
+    }
+
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, newContact]
+      }
+    })
+  }
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== contactId)
+      }
+    })
+  }
+
+  onFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  getFilter = () => {
+    const filter = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
+    );
+  };
+  
+  render() {
+    const { getFilter, deleteContact, onFilter, addContact } = this;
+    const filtredContacts = getFilter();
+    return (
+      <Layout>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={addContact} />
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} onChange={onFilter} />
+        <ContactList contacts={filtredContacts} onDelete={deleteContact} />
+      </Layout>
+    )
+  }
 }
-  return (
-    
-  );
-};
